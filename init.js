58ecb2c14de61e9e4f65946aba77c36d8c513d84
Good Caesar <christian@goodcaesar.com>
2012-03-15 06:17:41 +0000

    A few minor changes

--- init.js
+++ init.js
@@ -1,10 +1,9 @@
 $(function () {
 
-	$('#countdown').countdown({
-		until: new Date(2012, 3 - 1, 7), 
-		layout: '<ul>{y<}<li>{yn} {yl}</li>{y>}{o<}<li><span="count">{on}</span> <small>{ol}</small></span></li>{o>}' + 
-    '{d<}<li><span><span class="count">{dn}</span> <small>{dl}</small></span></li>{d>}{h<}<li><span><span class="count">{hn}</span> <small>{hl}</small></span></li>{h>}' + 
-    '{m<}<li><span><span class="count">{mn}</span> <small>{ml}</small></span></li>{m>}{s<}<li><span><span class="count">{sn}</span> <small>{sl}</small></span></li>{s>}</ul>'
-    });
-	
+	$('tbody tr').hover(function() {
+	  $(this).addClass('odd');
+	}, function() {
+	  $(this).removeClass('odd');
+	});
+		
 });
